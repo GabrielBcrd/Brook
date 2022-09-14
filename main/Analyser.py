d87beee881cd6f9c58a08e76d7d027371ee89d82
DESKTOP-KDMUNVG\Pouwpette <gabriel.bcrd@gmail.com>
2022-09-14 18:29:29 +0200

    test librosa

--- main/Analyser.py
+++ main/Analyser.py
@@ -3,10 +3,7 @@ from logging import config
 import wave
 
 import pygame
-import configparser
-import librosa
-from librosa import display
-import matplotlib.pyplot as plt
+
 
 
 fileConfig = ConfigParser()
@@ -19,11 +16,11 @@ pygame.display.set_caption("tes pygame")
 def analyser(conf,instruName):
    
 
-    pathSample = conf.get('InstrumentPath',instruName) + "\RimRag.wav"
+    pathSample = conf.get('InstrumentPath',instruName) + ""
     print(pathSample)
 
 #pygame package test
-    window_resolution = (640,480)
+    """  window_resolution = (640,480)
     window_surface = pygame.display.set_mode(window_resolution)
     song = pygame.mixer.Sound(pathSample)
     song.play(loops=10) 
@@ -34,25 +31,36 @@ def analyser(conf,instruName):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 launched = False
-
+    """
 #librosa package test
-    x , sr = librosa.load(pathSample, sr=44100)
-    print(type(x), type(sr))
+
+    import pandas as pd 
+    import numpy as np
+    import matplotlib.pylab as plt
+    import seaborn as sns 
+    from glob import glob
+    import librosa 
+    import librosa.display
+    import IPython.display as ipd
 
 
-    plt.figure(figsize=(14, 5))
+    audio_files = glob(pathSample + '\*.wav')
+  
+    ipd.Audio(audio_files[0])
 
-    chromagram = librosa.feature.chroma_stft(x, sr=sr, hop_length=hop_length)
-    plt.figure(figsize=(15, 5))
-    librosa.display.specshow(chromagram, x_axis='time', y_axis='chroma', hop_length='hop_length', cmap='coolwarm')
+    y, sr = librosa.load(audio_files[0])
+    print(y)
+    print(sr)
 
+    y_trimmed, _ = librosa.effects.trim(y,top_db=10)
 
+    pd.Series(y).plot(figsize=(10,5),
+                      lw = 1,
+                      title = "yo")
 
+    plt.show()
 
 
 # Appel Fonction pour test
 analyser(fileConfig,'rim')
-
-
-
 
